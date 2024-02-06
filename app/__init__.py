fcd85abdef47d4241b0dde1f953d15da4171b62a
Nicholas Leger <nicholasleger4@gmail.com>
2024-02-06 16:02:13 -0500

    phase2 1 complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 # !!START
-from flask import Flask
+from flask import (Flask, render_template)
 from .config import Config
+from .tweets import tweets
+from random import randint
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 # !!END
+
+@app.route("/")
+def index():
+    ri = randint(0, 4)
+    print(ri)
+    return render_template("index.html", tweet=tweets[ri])
