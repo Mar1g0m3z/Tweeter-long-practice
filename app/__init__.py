66910f84f8da05709a27397ea0cf0e7536f4460a
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-02-06 13:45:30 -0800

    style and feed route

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,14 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # !!END
 
+
 @app.route("/")
 def index():
     ri = randint(0, 4)
     print(ri)
     return render_template("index.html", tweet=tweets[ri])
+
+
+@app.route("/feed")
+def feed():
+    return render_template("feed.html", tweets=tweets)
